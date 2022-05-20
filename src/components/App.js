569eb8fbc7f799d65c43a5546e02e3e7d1ae200d
Annemarie <a.lucernoni0@gmail.com>
2022-05-19 22:20:09 -0700

    commit tree 95822b4e090d1cd270daded1b6ed769269f322dd

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-import ProjectList from "./ProjectList";
+import ProjectList from "./ProjectList"; 
 import user from "../data/user";
 
 function App() {
